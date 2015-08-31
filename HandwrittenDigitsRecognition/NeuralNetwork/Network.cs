cce57ed952f8958cc6185c63974ebcc5f926c636
Anna Oleksiak <anna.ol3ksiak@gmail.com>
2015-08-31 22:34:26 +0200

    Actually add files

---
+++ HandwrittenDigitsRecognition/NeuralNetwork/Network.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandwrittenDigitsRecognition.NeuralNetwork
+{
+    class Network
+    {
+        private int layerCount;
+        public int LayerCount
+        {
+            set { layerCount = value; }
+            get { return layerCount; }
+        }
+        private List<ILayerable> layers;
+        public List<ILayerable> Layers
+        {
+            set { layers = value; }
+            get { return layers; }
+        }
+
+        public Network(int[] inputs, int numOfLayers, int numOfNeurons)
+        {
+            LayerCount = numOfLayers;
+            Layers = new List<ILayerable>();
+            //Add input layer
+            Layers.Add(new InputLayer(inputs));
+            //Add neuron layers and link each to previous one
+            for (int i = 1; i < LayerCount; i++)
+            {
+                Layers.Add(new SigmoidNeuronLayer(numOfNeurons));
+                ((SigmoidNeuronLayer)Layers[i]).LinkBackToLayer(Layers[i - 1]);
+            }
+            //Add final (output) neuron layer - 10 neurons for 10 digits
+            Layers.Add(new SigmoidNeuronLayer(10));
+            ((SigmoidNeuronLayer)Layers[layerCount]).LinkBackToLayer(Layers[layerCount - 1]);
+        }
+
+        //TO DO - ADD LEARNING ELEMENTS TO NETWORK LEVEL
+    }
+}
